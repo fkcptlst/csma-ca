3fdbfdeb109b73666d013bb3083b45b683fd924b
hanchchch <cndghks15@gmail.com>
2021-12-15 17:09:26 +0900

    feat: configuration

---
+++ config.py
@@ -0,0 +1,59 @@
+from constant import (
+    MEGA,
+    ONE_SECOND,
+    SPEED_OF_LIGHT,
+)
+
+default_settings = {
+    "star_topology": True,
+    "with_rts": True,
+    "propagation_speed": SPEED_OF_LIGHT / 3,
+    "area_size": 50,
+    "station_count": 5,
+    "data_rate": 11 * MEGA,
+    "frame_rate": 1000,
+    "detect_range": 25,
+    "frame_size": 8 * 1500,
+    "backoff_min": 2,
+    "backoff_max": 1024,
+    "interval": 0.0,
+    "slot_time": 20,
+    "step": 10,
+    "max_time": ONE_SECOND // 10,
+    "area_size": 50,
+    "log_screen": True,
+    "sifs": 10,
+}
+
+station_count = [
+    {"station_count": 3},
+    {"station_count": 10},
+    {"station_count": 15},
+    {"station_count": 20},
+    {"station_count": 30},
+    {"station_count": 50},
+]
+
+backoff_min = [
+    {"backoff_min": 2},
+    {"backoff_min": 4},
+    {"backoff_min": 8},
+    {"backoff_min": 16},
+    {"backoff_min": 32},
+    {"backoff_min": 128},
+]
+
+frame_rate = [
+    {"frame_rate": 100},
+    {"frame_rate": 200},
+    {"frame_rate": 300},
+    {"frame_rate": 400},
+    {"frame_rate": 500},
+]
+
+various_settings = [
+    {**default_settings, **s, **b, **f}
+    for s in station_count
+    for b in backoff_min
+    for f in frame_rate
+]
--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 from typing import Type, Dict
 
 from dependency_injector.wiring import Provide, inject
-from constant import (
-    MEGA,
-    ONE_SECOND,
-    SPEED_OF_LIGHT,
-)
+
 from core.implements import (
     Station,
     Transmitter,
@@ -19,6 +15,7 @@ from core.implements import (
 )
 from core.time.line import TimeLine
 from core.container import DIContainer
+from config import default_settings
 from utils.log import logger_factory, station_notate, frame_notate
 
 
@@ -41,7 +38,7 @@ def main(
         slot_time=settings["slot_time"],
         with_rts=settings["with_rts"],
     )
-    timeline.set_after_tick(logger_factory(medium, settings["with_rts"]))
+    timeline.set_after_tick(logger_factory(medium, settings["frame_size"]))
     timeline.run()
 
 
@@ -50,24 +47,7 @@ if __name__ == "__main__":
     di_container.config.from_dict(
         {
             "settings": {
-                "star_topology": True,
-                "with_rts": True,
-                "propagation_speed": SPEED_OF_LIGHT / 3,
-                "area_size": 50,
-                "station_count": 5,
-                "data_rate": 11 * MEGA,
-                "frame_rate": 100,
-                "detect_range": 25,
-                "frame_size": 1500,
-                "backoff_min": 2,
-                "backoff_max": 1024,
-                "interval": 0.05,
-                "slot_time": 20,
-                "step": 20,
-                "max_time": 1 * ONE_SECOND,
-                "area_size": 50,
-                "log_screen": True,
-                "sifs": 10,
+                **default_settings,
             },
             "notation": [
                 {"instance": Station, "notation": station_notate},
